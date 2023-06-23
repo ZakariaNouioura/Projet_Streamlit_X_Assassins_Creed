e38163f84aef9b68c6a271c0dffa96ea47e336be
ZakariaNouioura <118918925+ZakariaNouioura@users.noreply.github.com>
2023-06-23 10:23:06 +0200

    openxyl

--- Pages/1_L_histoire.py
+++ Pages/1_L_histoire.py
@@ -2,6 +2,7 @@ import pandas as pd
 import io
 import requests
 import streamlit as st
+import openpyxl
 
 url = "https://github.com/ZakariaNouioura/Projet_Streamlit_X_Assassins_Creed/raw/main/DataFrame/Les_jeux.xlsx"
 response = requests.get(url)
